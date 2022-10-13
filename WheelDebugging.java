dca7db748df7f32ae720203599b387e1dfe39b08
Vector A <Vector5233@gmail.com>
2022-10-13 16:36:45 -0400

    Debugging branch WheelDebugging - attempts to debug leftFront wheel turning wrong way.

--- WheelDebugging.java
+++ WheelDebugging.java
@@ -5,27 +5,27 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name ="IanDriving", group = "learning")
-
-public class IanDriving extends OpMode {
+@TeleOp(name="WheelDebugging", group="debug")
+public class WheelDebugging extends OpMode {
     DcMotor leftFront, leftRear, rightFront, rightRear;
-    public void init(){
+    public void init() {
         leftFront = hardwareMap.dcMotor.get("leftFront");
-        rightFront = hardwareMap.dcMotor.get("rightFront");
         leftRear = hardwareMap.dcMotor.get("leftRear");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
         rightRear = hardwareMap.dcMotor.get("rightRear");
 
         rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
-
     }
-    public void loop(){
-        double leftPower = -gamepad1.left_stick_y;
-        double rightPower = -gamepad1.right_stick_y;
 
-        leftFront.setPower(leftPower);
-        leftRear.setPower(leftPower);
-        rightFront.setPower(rightPower);
-        rightRear.setPower(rightPower);
+    public void loop() {
+        double motorPower = -gamepad1.left_stick_y;
+        telemetry.addData("power: " , motorPower);
+
+        leftFront.setPower(motorPower);
+        leftRear.setPower(motorPower);
+        rightFront.setPower(motorPower);
+        rightRear.setPower(motorPower);
+
     }
 }

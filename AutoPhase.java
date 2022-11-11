3d49752dba694d07ee7cfdee9dac9d36a10a6e13
Vector A <Vector5233@gmail.com>
2022-11-11 13:54:20 -0500

    TeleOp arm rotation divided by 10 11/9/22

--- AutoPhase.java
+++ AutoPhase.java
@@ -1,13 +1,15 @@
 package org.firstinspires.ftc.teamcode.VectorCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-@Autonomous(name = "AutoPhase", group = "Auto")
-class AutoPhase extends AutoTemplate {
+
+//@Autonomous(name = "AutoPhase", group = "Auto")
+/*class AutoPhase extends AutoTemplate {
     public void runOpMode() {
         initialize();
         telemetry.addLine("initialized!");
         telemetry.update();
         waitForStart();
     }
-}
+}*/

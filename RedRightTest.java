74774490adf32b4d6ae81aba05d81c960caa234b
VectorB <Vector5233@gmail.com>
2022-12-14 13:35:28 -0500

    RedRightTest position finished

--- RedRightTest.java
+++ RedRightTest.java
@@ -10,5 +10,50 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name= "RedRight", group = "Red", preselectTeleOp = "AgnesTeleOp")
-public class RedRight extends AutoTemplate {
+@Autonomous(name= "RedRightTest", group = "Red", preselectTeleOp = "AgnesTeleOp")
+public class RedRightTest extends AutoTemplate {
+
+    final double FIRST_FORWARD = 14.72;
+    final double CENTER_FORWARD = 25;
+    final double RIGHT_AND_LEFT_FORWARD = 11;
+    final double STRAFE_LEFT = 25.5;
+    final double STRAFE_RIGHT = 25;
+    final double FINAL_FORWARD = 12;
+
+    public void runOpMode() {
+        initialize();
+
+        telemetry.addLine("initialized!");
+        telemetry.update();
+
+        while (!isStarted() && !isStopRequested()) {
+            detectAprilTags();
+        }
+
+        reportAprilTags();
+
+        Trajectory initialForwardTrajectory = drive.trajectoryBuilder(new Pose2d())
+                .forward(FIRST_FORWARD)
+                .build();
+        drive.followTrajectory(initialForwardTrajectory);
+
+        deliverPreCone();
+
+        Trajectory redRightTallPole = drive.trajectoryBuilder(initialForwardTrajectory.end())
+                .lineToSplineHeading(new Pose2d(56.000, -9.132, 4.5)) //x coordinate changelog: 61.003 --> 59.000 --> 56.000
+                .build();
+        drive.followTrajectory(redRightTallPole);
+
+
+    }
+
+    public void deliverPreCone(){
+        sleep(500);
+        autoDeliveryLeft.setPosition(DELIVER_LEFT);
+        autoDeliveryRight.setPosition(DELIVER_RIGHT);
+        sleep(1000);
+        autoDeliveryLeft.setPosition( RECOVER_LEFT);
+        autoDeliveryRight.setPosition( RECOVER_RIGHT);
+        sleep(1000);
+    }
+}

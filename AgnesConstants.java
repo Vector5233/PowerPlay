012fd33c1c57af4bfed5aba08f62ae46bfa0024e
VectorB <Vector5233@gmail.com>
2023-01-07 12:15:39 -0500

    added MAXARMANGLEDEGREE and MINARMANGLEDEGREE

--- AgnesConstants.java
+++ AgnesConstants.java
@@ -33,6 +33,8 @@ public class AgnesConstants {
     public static final double MINAUTOTICKS = -933;
     public static final double MAXTELEOPTICKS = 515;
     public static final double MINTELEOPTICKS = -691;
+    public static final double MAXARMANGLEDEGREE = 140;
+    public static final double MINARMANGLEDEGREE = 0;
     public static final double p = .004;
     public static final double i = .2;
     public static final double d = .0008;

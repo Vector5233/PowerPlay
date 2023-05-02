74978e24048ea919baf08be60a3cb737399b7aff
VectorB <Vector5233@gmail.com>
2023-05-02 13:50:50 -0400

    Made ScorpianTeleOp

--- ScorpionTeleOp.java
+++ ScorpionTeleOp.java
@@ -1,41 +1,62 @@
 package org.firstinspires.ftc.teamcode.VectorCode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
-import android.util.Log;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @TeleOp (name = "ScorpionTeleOp", group = "robot")
 public class ScorpionTeleOp {
-    DcMotor baseLeft, baseRight, rotationOne, rotationTwo;
+    DcMotor shoulderLeft, shoulderRight, elbowOne, elbowTwo;
 
     public void init() {
-        baseLeft = hardwareMap.dcMotor.get("baseLeft");
-        baseRight = hardwareMap.dcMotor.get("baseRight");
-        rotationOne= hardwareMap.dcMotor.get("rotationOne");
-        rotationTwo = hardwareMap.dcMotor.get("rotationTwo");
+        shoulderLeft = hardwareMap.dcMotor.get("shoulderLeft");
+        shoulderRight = hardwareMap.dcMotor.get("shoulderRight");
+        elbowOne = hardwareMap.dcMotor.get("elbowOne");
+        elbowTwo = hardwareMap.dcMotor.get("elbowTwo");
 
-        baseLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        baseLeft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        shoulderLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        shoulderLeft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
-        baseRight.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        baseRight.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        shoulderRight.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        shoulderRight.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
-        rotationOne.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        rotationOne.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        elbowOne.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        elbowOne.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
-        rotationTwo.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        rotationTwo.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-
-
+        elbowTwo.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        elbowTwo.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
    }
 
+   public void loop(){
+        setPositionOne();
+        setPositionTwo();
+        setPositionThree();
+        setPositionFour();
+   }
 
-
-
+   public void setPositionOne(){
+        if (gamepad2.a){
+            telemetry.addLine("position 1");
+        }
+   }
+   public void setPositionTwo(){
+        if (gamepad2.b){
+            telemetry.addLine("position 2");
+        }
+   }
+   public void setPositionThree(){
+        if (gamepad2.y){
+            telemetry.addLine("position 3");
+        }
+   }
+   public void setPositionFour(){
+        if (gamepad2.x){
+            telemetry.addLine("position 4");
+        }
+   }
 
 
 
